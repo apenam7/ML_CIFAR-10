bb292f8cc0dcbb2fb7ec7ca8bf5df50fc0f3f950
Ariel Pena-Martinez <apenam7@gmail.com>
2020-04-03 22:08:30 -0400

    Adding changes

--- cifar_10.py
+++ cifar_10.py
@@ -1,6 +1,16 @@
+import sys
+import matplotlib.pyplot as plt
 from keras.datasets import cifar10
-from keras.utils.np_utils import to_categorical
-from keras.engine.sequential import Sequential
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+import tensorflow as tf
+
+print(tf.config.list_physical_devices('GPU'))
 
 # load train and test dataset
 def load_dataset():
@@ -28,3 +38,46 @@ def define_model():
 	model = Sequential()
 	# ...
 	return model
+
+
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+	# plot loss
+	plt.subplot(211)
+	plt.title('Cross Entropy Loss')
+	plt.plot(history.history['loss'], color='blue', label='train')
+	plt.plot(history.history['val_loss'], color='orange', label='test')
+	# plot accuracy
+	plt.subplot(212)
+	plt.title('Classification Accuracy')
+	plt.plot(history.history['accuracy'], color='blue', label='train')
+	plt.plot(history.history['val_accuracy'], color='orange', label='test')
+	# save plot to file
+	filename = sys.argv[0].split('/')[-1]
+	plt.savefig(filename + '_plot.png')
+	plt.close()
+
+# run the test harness for evaluating a given model.
+# It is better to use a separate validation dataset, e.g. by splitting the train dataset 
+# into train and validation sets. We will not split the data in this case, and instead use 
+# the test dataset as a validation dataset to keep the example simple.
+# This function involves all previously defined functions.
+def run_test_harness():
+	# load dataset
+	trainX, trainY, testX, testY = load_dataset()
+	# prepare pixel data
+	trainX, testX = prep_pixels(trainX, testX)
+	# define model
+	model = define_model()
+	# fit model
+	history = model.fit(trainX, trainY, epochs=100, batch_size=64, validation_data=(testX, testY), verbose=0)
+	# evaluate model
+	_, acc = model.evaluate(testX, testY, verbose=0)
+	print('> %.3f' % (acc * 100.0))
+	# learning curves
+	summarize_diagnostics(history)
+
+# entry point, run the test harness.
+# This test harness can evaluate any CNN models we may wish to evaluate on the 
+# CIFAR-10 dataset and can run on the CPU or GPU.
+run_test_harness()
